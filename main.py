089543979b256ed6de63334b4e7877b87a630d3b
Arthur P. Cortes <arthurpc02@gmail.com>
2025-04-05 17:39:12 -0300

    feat: add second function to get info about all games in one archive, and print some meaningful information about it.

--- main.py
+++ main.py
@@ -10,21 +10,28 @@ https://api.chess.com/pub/player/{username}/games/archives
 """
 
 import requests
+import json
 
 HEADERS = {
     "User-Agent": "MyChessApp/0.1 (contact: arthurpc02@gmail.com)"
 }
 
-def get_games_by_month_year(player_name: str, month: str, year: str):
-
-    print(f'Get games by month and year')
 
 def get_archives(username):
     url = f"https://api.chess.com/pub/player/{username}/games/archives"
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
     data = response.json()
+    print(json.dumps(data, indent=2))
     return data["archives"]
+
+
+def get_games(archive_url):
+    response = requests.get(archive_url, headers=HEADERS)
+    response.raise_for_status()
+    print(json.dumps(response.json(), indent=2))
+    return response.json()["games"]
+
 
 if __name__ == '__main__':
     # get_games_by_month_year('arthurpc02', '11', '2023')
@@ -32,5 +39,11 @@ if __name__ == '__main__':
     archives = get_archives("arthurpc02")
     print(f"Found {len(archives)} archives.")
 
+    # fetch the most recent month:
+    latest = archives[-2]
+    print(f"fetching games from {latest}")
+    games = get_games(latest)
+    print(f"total games: {len(games)}.")
 
-
+    for g in games:
+        print(f"{g["white"]["username"]}[{g["white"]["result"]}] vs {g["black"]["username"]}[{g["black"]["result"]}]: {g["url"]}")
