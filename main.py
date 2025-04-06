4ec91f3641a3b8c6622bfefb5d227c4c005a6c77
Arthur P. Cortes <arthurpc02@gmail.com>
2025-04-06 20:03:09 -0300

    feat: make async requests.

--- main.py
+++ main.py
@@ -12,48 +12,53 @@ import time
 
 import requests
 import json
+import asyncio
+import aiohttp
 
 HEADERS = {
     "User-Agent": "MyChessApp/0.1 (contact: arthurpc02@gmail.com)"
 }
 
-def get_archives(username):
+async def get_archives(session, username):
     url = f"https://api.chess.com/pub/player/{username}/games/archives"
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    data = response.json()
-    # print(json.dumps(data, indent=2))
-    return data["archives"]
+    async with session.get(url, headers=HEADERS) as response:
+        response.raise_for_status()
+        data = await response.json()
+        # print(json.dumps(data, indent=2))
+        return data["archives"]
 
 
-def get_games(archive_url):
-    response = requests.get(archive_url, headers=HEADERS)
-    response.raise_for_status()
-    # print(json.dumps(response.json(), indent=2))
-    return response.json()["games"]
+async def get_games(session, archive_url):
+    async with session.get(archive_url, headers=HEADERS) as response:
+        response.raise_for_status()
+        data = await response.json()
+        # print(json.dumps(response.json(), indent=2))
+        return data["games"]
 
 
-def get_all_games_from_all_months(all_archives: list[str]):
-    all_games = []
-    for archive in all_archives:
-        games_monthly = get_games(archive)
-        for games in games_monthly:
-            all_games.append(games)
-    return all_games
+async def get_all_games_from_all_months(session, all_archives: list[str]):
+    tasks = [get_games(session, archive_url) for archive_url in all_archives]
+    all_monthly_games = await asyncio.gather(*tasks)
+    return [game for monthly in all_monthly_games for game in monthly]
 
 
-if __name__ == '__main__':
+async def main():
     start_time = time.perf_counter()
 
-    archives = get_archives("arthurpc02")
-    print(f"Found {len(archives)} archives.")
+    async with aiohttp.ClientSession() as session:
+        archives = await get_archives(session, "arthurpc02")
+        print(f"Found {len(archives)} archives.")
+        all_games = await get_all_games_from_all_months(session, archives)
 
-    all_games = get_all_games_from_all_months(archives)
     end_time = time.perf_counter()
 
     for g in all_games:
         print(f"{g["white"]["username"]}[{g["white"]["result"]}] vs {g["black"]["username"]}[{g["black"]["result"]}]: {g["url"]}")
 
-
     print("\n##########")
     print(f"I/O tasks total time: {end_time-start_time:.2f} seconds.")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
